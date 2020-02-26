df3645bd8c790dde484d14c8886c6abea591bc2b
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-02-26 19:16:43 +0900

    Add files via upload

--- json2cola.py
+++ json2cola.py
@@ -1,11 +1,12 @@
 import json
 import os
-import argparse
-import io
-import urllib.request
+import random
 
 input_folder_json="./wikijson"
 output_file_cola="./wikicola.txt"
+
+SAMPLE_NUM=50
+SENTENCE_MIN=10
 
 def ffzk(input_dir):#Relative directory for all existing files
     imgname_array=[];input_dir=input_dir.strip("\"\'")
@@ -15,14 +16,15 @@ def ffzk(input_dir):#Relative directory for all existing files
     return imgname_array
 
 if __name__ == '__main__':
-    imgname_array=ffzk(input_folder_json)
+    imgname_array=random.sample(ffzk(input_folder_json),SAMPLE_NUM)
     
     for imgname in imgname_array:
         with open(imgname, "r",encoding="utf-8") as fp:
             json_load = json.load(fp)
             for title in json_load.keys():
                 for sentence in json_load[title].translate(str.maketrans("。",".","\n")).split("."):
+                    if len(sentence)<SENTENCE_MIN:continue#←filter
                     with open(output_file_cola, "a",encoding="utf-8") as fp:
-                        fp.write(title+"\t"+"1"+"\t_\t"+sentence+"\n")
+                        fp.write(title+"\t"+"1"+"\t_\t"+sentence.strip()+"\n")
     print("finish!")
     
