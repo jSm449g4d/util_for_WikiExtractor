377ed12475c5416b03509ebc7da8f63b4c6ff931
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-03-02 16:25:07 +0900

    Add files via upload

--- makemodel_ft.py
+++ makemodel_ft.py
@@ -6,7 +6,7 @@ starttime=str(datetime.now())
 print("start")
 
 model = fasttext.train_unsupervised('wikinoun.txt',lr=0.025,dim=200,ws=5,minCount=5,
-                                    thread=multiprocessing.cpu_count(),epoch=5,neg=3,)
+                                    thread=multiprocessing.cpu_count()-1,epoch=5,neg=3,)
 model.save_model("wikinoun.bin")
 
 print("Finish!"+starttime+"→"+str(datetime.now()))
--- makemodel_w2v.py
+++ makemodel_w2v.py
@@ -12,7 +12,7 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 
 corpus =word2vec.LineSentence('wikinoun.txt')
 model = word2vec.Word2Vec(corpus,sg=1,alpha=0.025, size=200, window=5,min_count=5,
-                          workers=multiprocessing.cpu_count(),iter=5,negative=3,)
+                          workers=multiprocessing.cpu_count()-1,iter=5,negative=3,)
 model.save("wikinoun.model")
 
 print("Finish!"+starttime+"→"+str(datetime.now()))
